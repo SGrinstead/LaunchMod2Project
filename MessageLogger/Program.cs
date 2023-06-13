f50427e437ffebba5db3f0a9aa1dd8ff44049e1c
SGrinstead <seth.grinstead3299@gmail.com>
2023-06-13 09:25:33 -0700

    refactoring methods

--- MessageLogger/Program.cs
+++ MessageLogger/Program.cs
@@ -1,48 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MessageLogger.Data;
 using MessageLogger.Models;
+using System.Xml.Linq;
 
-//add a using context block around most of the program file
 using (var context = new MessageLoggerContext())
 {
-    //foreach loops with users will become context.users
     Console.WriteLine("Welcome to Message Logger!");
-    Console.WriteLine();
-    Console.WriteLine("Let's create a user pofile for you.");
-    Console.Write("What is your name? ");
-    string name = Console.ReadLine();
-    Console.Write("What is your username? (one word, no spaces!) ");
-    string username = Console.ReadLine();
-    //context.users.add then savechanges to add created user to the database
-    User currentUser = new User(name, username);
-    context.Users.Add(currentUser);
-    context.SaveChanges();
+    RunProgram(context);
+}
 
+static string Format(string s)
+{
+    return s.ToLower().Replace(" ", "");
+}
+
+static void HelpMessage()
+{
     Console.WriteLine();
     Console.WriteLine("To log out of your user profile, enter `log out`.");
 
     Console.WriteLine();
     Console.Write("Add a message (or `quit` to exit): ");
+}
 
+static void RunProgram(MessageLoggerContext context)
+{
+    User currentUser = NewUser(context);
+
+    HelpMessage();
     string userInput = Console.ReadLine();
-    //this can be removed to be replaced by context.users
-    //List<User> users = new List<User>() { user };
 
-    while (userInput.ToLower() != "quit")
+    while (Format(userInput) != "quit")
     {
-        while (userInput.ToLower() != "log out")
+        while (Format(userInput) != "logout")
         {
             currentUser.Messages.Add(new Message(userInput));
-            //add a savechanges here
             context.SaveChanges();
 
-            foreach (var message in currentUser.Messages)
-            {
-                Console.WriteLine($"{currentUser.Name} {message.CreatedAt:t}: {message.Content}");
-            }
+            currentUser.PrintMessages();
 
             Console.Write("Add a message: ");
-
             userInput = Console.ReadLine();
             Console.WriteLine();
         }
@@ -51,32 +48,14 @@ using (var context = new MessageLoggerContext())
         userInput = Console.ReadLine();
         if (userInput.ToLower() == "new")
         {
-            Console.Write("What is your name? ");
-            name = Console.ReadLine();
-            Console.Write("What is your username? (one word, no spaces!) ");
-            username = Console.ReadLine();
-            //add to context.users and savechanges
-            currentUser = new User(name, username);
-            context.Users.Add(currentUser);
-            context.SaveChanges();
+            currentUser = NewUser(context);
+
             Console.Write("Add a message: ");
-
             userInput = Console.ReadLine();
-
         }
-        else if (userInput.ToLower() == "existing")
+        else if (Format(userInput) == "existing")
         {
-            //in the future this will find the user in context.users
-            Console.Write("What is your username? ");
-            username = Console.ReadLine();
-            currentUser = null;
-            foreach (var existingUser in context.Users)
-            {
-                if (existingUser.Username == username)
-                {
-                    currentUser = existingUser;
-                }
-            }
+            currentUser = LogIn(context);
 
             if (currentUser != null)
             {
@@ -92,11 +71,43 @@ using (var context = new MessageLoggerContext())
         }
 
     }
+    ClosingInfo(context);
+}
 
+static User NewUser(MessageLoggerContext context)
+{
+    Console.WriteLine();
+    Console.WriteLine("Let's create a user pofile for you.");
+    Console.Write("What is your name? ");
+    string name = Console.ReadLine();
+    Console.Write("What is your username? (one word, no spaces!) ");
+    string username = Console.ReadLine();
+    User currentUser = new User(name, username);
+    context.Users.Add(currentUser);
+    context.SaveChanges();
+    return currentUser;
+}
 
+static void ClosingInfo(MessageLoggerContext context)
+{
     Console.WriteLine("Thanks for using Message Logger!");
     foreach (var u in context.Users)
     {
         Console.WriteLine($"{u.Name} wrote {u.Messages.Count} messages.");
     }
+}
+
+static User LogIn(MessageLoggerContext context)
+{
+    Console.Write("What is your username? ");
+    string username = Console.ReadLine();
+    User currentUser = null;
+    foreach (var existingUser in context.Users)
+    {
+        if (existingUser.Username == username)
+        {
+            currentUser = existingUser;
+        }
+    }
+    return currentUser;
 }
