41263820f9a31d148e8dc3b19eeaf33deeb58916
SGrinstead <seth.grinstead3299@gmail.com>
2023-06-14 13:20:14 -0700

    added statistics methods and updated program to run them at the end

--- MessageLogger/Program.cs
+++ MessageLogger/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MessageLogger.Data;
 using MessageLogger.Models;
+using System.Runtime.CompilerServices;
 using System.Xml.Linq;
 
 using (var context = new MessageLoggerContext())
@@ -77,9 +78,31 @@ static User NewUser(MessageLoggerContext context)
 static void ClosingInfo(MessageLoggerContext context)
 {
     Console.WriteLine("Thanks for using Message Logger!");
-    foreach (var u in context.Users)
+    Console.WriteLine();
+    Console.WriteLine("Please enter a number to see some neat statistics:");
+    Console.WriteLine("1: The number of messages each user has written");
+    Console.WriteLine("2: Users ordered by the number of messages they have written");
+    Console.WriteLine("3: The hour with the most messages written");
+    Console.WriteLine("4: The most common word users wrote");
+    Console.WriteLine("5: Quit");
+    string userInput = Console.ReadLine();
+    switch (userInput) 
     {
-        Console.WriteLine($"{u.Name} has written {u.Messages.Count} messages.");
+        case "1":
+            MessageCountByUser(context); 
+            break;
+        case "2":
+            UsersOrderedByMessageCount(context);
+            break;
+        case "3":
+            HourWithMostMessages(context);
+            break;
+        case "4":
+            MostCommonWord(context);
+            break;
+        default:
+            Console.WriteLine("Goodbye"); 
+            break;
     }
 }
 
@@ -111,4 +134,82 @@ static User LogIn(MessageLoggerContext context)
         return GetUser(context);
     }
     else return null;
+}
+
+static void UsersOrderedByMessageCount(MessageLoggerContext context)
+{
+    var orderedUsers = context.Users.OrderByDescending
+        (user => user.Messages.Count());
+    foreach (var user in orderedUsers)
+    {
+        Console.WriteLine($"{user.Name}: {user.Messages.Count()} messages");
+    }
+}
+
+static void HourWithMostMessages(MessageLoggerContext context)
+{
+    var hours = context.Messages.GroupBy(message => message.CreatedAt.ToLocalTime().Hour);
+    int hourWithMost = 0;
+    int messageCount = 0;
+    foreach(var hour in hours)
+    {
+        if (hour.Count() > messageCount)
+        {
+            hourWithMost = hour.Key;
+            messageCount = hour.Count();
+        }
+    }
+    Console.WriteLine($"Hour {hourWithMost} had the most messsages with a count of {messageCount}");
+}
+
+static string AllWords(MessageLoggerContext context)
+{
+    string allWords = "";
+    var characterList = new List<string> { " ", "!", "' ", "?", ";", ":", ".", "/" };
+    foreach(var message in context.Messages)
+    {
+        allWords += message.Content + " ";
+    }
+    foreach(string character in characterList)
+    {
+        if (allWords.Contains(character))
+        {
+            allWords = allWords.Replace(character, ",");
+        }
+    }
+    Console.WriteLine(allWords);
+    return allWords;
+}
+
+static void MostCommonWord(MessageLoggerContext context)
+{
+    string allWords = AllWords(context);
+    var split = allWords.Split(",");
+    var wordCount = new Dictionary<string, int>();
+
+    foreach(string word in split)
+    {
+        Console.WriteLine(word);
+        string lowerWord = word.ToLower();
+        if (string.IsNullOrEmpty(lowerWord))
+        {
+            continue;
+        }
+        if (!wordCount.ContainsKey(lowerWord))
+        {
+            wordCount.Add(lowerWord, 0);
+        }
+        wordCount[lowerWord]++;
+    }
+    var maxPair = wordCount.First(word => word.Value == wordCount.Max(word => word.Value));
+
+    Console.WriteLine($"The most common word is {maxPair.Key} with {maxPair.Value} uses");
+}
+
+static void MessageCountByUser(MessageLoggerContext context)
+{
+    foreach (var u in context.Users)
+    {
+        Console.WriteLine($"{u.Name} has written {u.Messages.Count} messages.");
+    }
 }
